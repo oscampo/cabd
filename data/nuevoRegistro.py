561fcc9324f2cb47c0ab9cbca2bc3356e31c221d
oscampo <oscampo@hotmail.com>
2022-06-10 17:45:13 -0500

    Update nuevoRegistro.py

--- data/nuevoRegistro.py
+++ data/nuevoRegistro.py
@@ -1,5 +1,8 @@
 import sqlite3
+import os.path
 
-conexion=sqlite3.connect('./data/people.db')
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, 'people.db')
+conexion=sqlite3.connect(db_path)
 conexion.execute("insert into people(MINISTERIO,NOMBRE,EDAD,DOC_ID,PHONE,T_CR,CURSOS) values (?,?,?,?,?,?,?)", ("Alabanza", "Ildebrancio Cantor",25,456123,3576842,"5 años","ADN,LLP"))                    
 conexion.close()
