c63a10d9ce89bce1a72c96ae11ff814289d5a44d
Oscar Campo <oscampo@hotmail.com>
2022-06-10 21:28:11 -0500

    updated creaTabla.html & nuevoRegistro.py

--- data/nuevoRegistro.py
+++ data/nuevoRegistro.py
@@ -2,6 +2,6 @@ import sqlite3
 
 conn = sqlite3.connect('people.db')
 
-cursor = conn.execute("insert into people(MINISTERIO,NOMBRE,EDAD,DOC_ID,PHONE,T_CR,CURSOS) values (?,?,?,?,?,?,?)", ("Alabanza", "Ildebrancio Cantor",25,456123,3576842,"5 años","ADN,LLP"))
+cursor = conn.execute("insert into People values (?,?,?,?,?,?,?,?)", (3,"Alabanza", "Ildebrancio Cantor",25,456123,3576842,"5 años","ADN,LLP"))
 
 conn.close()
