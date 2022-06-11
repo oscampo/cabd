700cc1acbdd485192ed09d2be48ad542e25711da
Oscar Campo <oscampo@hotmail.com>
2022-06-10 22:29:10 -0500

    Update nuevoRegistro.py

--- data/nuevoRegistro.py
+++ data/nuevoRegistro.py
@@ -4,4 +4,5 @@ conn = sqlite3.connect('./data/people.db')
 
 conn.execute('''INSERT INTO People (ID,MINISTERIO,NOMBRE,EDAD,DOC_ID,PHONE,T_CR,CURSOS) VALUES (5,"Alabanza", "Ildebrancio Cantor",25,456123,3576842,"5 años","ADN,LLP")''');
 conn.commit()
+print("saved!")
 conn.close()
